31e61877c6c2b5aca83ba2fb847a8d5b2f35ba96
abbym-bristol <abby.morris@bristol.ac.uk>
2026-04-02 15:04:09 +0100

    feat: move week schedule to separate part of dict

--- event_reformat.py
+++ event_reformat.py
@@ -31,6 +31,7 @@ def reformat_schedule(payload):
     schedule["type"] = data.get("type")
     schedule["zone"] = data.get("zone")
     schedule["mode"] = data.get("mode")
+    week = {}
     for day in ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]:
         day_schedule = []
         for event in week_schedule:
@@ -42,12 +43,14 @@ def reformat_schedule(payload):
         # get hours and temp for each day in schedule
         hours = []
         C = []
+        
         for event in day_schedule:
             hours.append(int(event["time"].split(":")[0])) # extract hour from time string
             C.append(event.get("C", event.get("preset")))
         
         # reformat schedule for each day as dictionary with hours and temp    
-        schedule[day] = {"hours": hours, "C": C}
+        week[day] = {"hours": hours, "C": C}
+    schedule["schedule"] = week
         
     return schedule
 
@@ -78,7 +81,8 @@ def get_override(payload):
     return None
 
 # loop through each house and reformat schedule and override, then save to new csv file
-Path("PSTData5_reformat").mkdir(exist_ok = True) # create new directory for reformatted files
+dir = "PSTData5_reformat"
+Path(dir).mkdir(exist_ok = True) # create new directory for reformatted files
 
 for house_id, df_house in houses.items():
     df = df_house.copy()
@@ -92,7 +96,7 @@ for house_id, df_house in houses.items():
     df.loc[mask,"Override"] = schedule_payloads.apply(get_override) # create new override column for 'Schedule' rows
     df.loc[mask, "Payload"] = schedule_payloads.apply(reformat_schedule) # reformat week schedule for 'Schedule' rows
 
-    df.to_csv(f"PSTData5_reformat/{house_id}_reformat_events.csv", index = False) # save to new csv file
+    df.to_csv(f"{dir}/{house_id}_reformat_events.csv", index = False) # save to new csv file
     
     print(f"{house_id} complete")
     
